3109cc8ea62ce2b8b55e4956789cb2659fb54b05
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    Resolved merge conflict and added new project text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Ryan Oliver and this is my project </p>
+        <p> My name is and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
       </header>
     </div>
   );
